dc0610bc7d5aea392d766004744b6ea015d41520
Дмитрий <anosov1995@yandex.ru>
2022-06-13 13:56:43 +0300

    Сделал первые API тесты

---
+++ src/test/java/ds/anosov/FirstTestAPI.java
@@ -0,0 +1,78 @@
+package ds.anosov;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class FirstTestAPI {
+
+    @Test
+    public void singleUserGetTest() {
+        get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.first_name",is("Janet"));
+    }
+
+    @Test
+    public void deleteTest() {
+        delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .statusCode(204);
+    }
+
+    @Test
+    public void missingPasswordPostTest() {
+        String requestBody = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(requestBody) //сюда вставляется тело отправляемого POST запроса
+                .contentType(JSON) //обязательно нужно указывать тип (img, video, JSON, xml...)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));//это проверка ответа
+    }
+
+    @Test
+    public void createPostTest() {
+        String requestBodyTest = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .body(requestBodyTest)
+                .contentType(JSON)
+                .post("https://reqres.in/api/users")
+                .then().log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"))
+                .body("id", notNullValue())
+                .body("createdAt", notNullValue());
+    }
+
+    @Test
+    public void updatePutTest() {
+        String requestBodyPutTest = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .body(requestBodyPutTest)
+                .contentType(JSON)
+                .put("https://reqres.in/api/users/2")
+                .then().log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"))
+                .body("updatedAt", notNullValue());
+    }
+}
---
+++ src/test/java/ds/anosov/SimpleTestsAPI.java
@@ -0,0 +1,73 @@
+package ds.anosov;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTestsAPI {
+
+    @Test
+    public void simpleTest() {
+        given()
+                .log().uri() // это логи запроса
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status() // это логи ответа
+                .body("used", is(0));
+    }
+
+
+    @Test
+    public void simpleTestWithoutGiven() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("used", is(0));
+    }
+
+    @Test
+    public void simpleTest2() {
+       int testUrl = get("https://selenoid.autotests.cloud/status")
+                .then().log().all()
+               .extract().path("pending"); //сюда пишется ключ, который будет получен из Json документа
+
+    /* Пример, если надо получить total в path(total) пишется total
+         {
+               total: 20,
+                       used: 0,
+                 queued: 0,
+                    pending: 0,
+    */
+
+       int answerTest = 0;
+       assertEquals(testUrl, answerTest);
+    }
+
+    @Test
+    public void simpleTest3() {
+        get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void simpleTest4() {
+        get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void simpleTest5() {
+        given()
+                .auth().basic("user1","1234")
+                .get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(200);
+    }
+
+}
